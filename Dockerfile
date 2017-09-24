9b0527b8669e8bb7eb91b4811778689541beb6aa
Dev khadka <devbahadurkhadka@SMS-EXE-Dev.local>
2017-09-24 18:06:50 +0545

    adding project

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM ubuntuwithssh:1.0.2
+
+#update package database 
+#install php and php extensions required by laravel
+#PHP >= 5.5.9
+#OpenSSL PHP Extension
+#PDO PHP Extension
+#Mbstring PHP Extension
+#Tokenizer PHP Extension
+
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php7.0-mbstring php7.0-zip php7.0-xml php7.0-mysql curl php7.0-curl
+
+#Install NodeJS
+RUN apt-get install -y nodejs npm
+
+#install composer, which is dependency manager for laravel
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+
+
+#restart apache server and enable url rewrite mode
+RUN service apache2 stop && a2enmod rewrite
+
+
+#add virtual host configuration of the site
+ADD 000-default.conf /etc/apache2/sites-available/000-default.conf
+
+
+#Set the ENV vars
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+ENV APACHE_LOCK_DIR /var/lock/apache2
+
+
+ADD entrypoint.sh /root/entrypoint.sh
+RUN chmod 777 /root/entrypoint.sh
+ENTRYPOINT /root/entrypoint.sh
+
+
+
+EXPOSE 80
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+# Don't remove, this command is needed to start ssh server when docker is started
+/usr/sbin/sshd;
+
+cd /var/www/html/project;
+# Creates laravel project if doesn't exists already
+if [ -f composer.json ]; then
+	echo "Laravel project exists";
+	if [ -d vendor ]; then
+		echo "Vendor folder exists not installing composer packages.";
+	else
+		echo "Install composer packages";
+		composer install -vv;
+	fi	
+else
+	echo "Laravel project doesn't exist, creating new ...";
+ 	composer -vvv create-project laravel/laravel /var/www/html/project --prefer-dist;
+fi
+
+
+php artisan migrate;
+
+# Install Node dependencies from package.json file if exists
+if [ -f /var/www/html/project/package.json ]; then
+	if [ -d /var/www/html/project/node_modules ]; then
+		echo "Node modules already installed";
+	else
+		echo "Install node modules";
+		npm --prefix /var/www/html/project install /var/www/html/project;
+	fi	
+else
+	echo "No package.json file";
+fi
+
+
+# make following folders accessible to apache
+chgrp -R www-data /var/www/html/project/storage;
+chgrp -R www-data /var/www/html/project/bootstrap/cache;
+chmod -R 777 /var/www/html/project/storage;
+
+
+#Launch apache in the foreground
+#We do this in the forground so that Docker can watch
+#the process to detect if it has crashed
+apache2 -DFOREGROUND;
